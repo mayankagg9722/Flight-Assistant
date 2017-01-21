7b2fac0c6e161bc69e45f505f6b4648f8a4cb36d
mayankagg9722 <mayankagg9722@gmail.com>
2017-01-22 02:11:17 +0530

    added cab find route

--- routes/cab.js
+++ routes/cab.js
@@ -160,5 +160,100 @@ router.get('/cabrequested', function (req, res, next) {
     }
 });
 
+router.get('/getcabinfo', function (req, res, next) {
+    if (!req.user) {
+        res.status = 401;
+        res.json({
+            status: false,
+            redirect: 'login',
+            message: "Authentication Failed"
+        });
+    }
+    else {
+        getCab.findOne({ username: req.user.username }, function (err, data) {
+            if (err) {
+                res.status = 401;
+                res.json({
+                    status: false,
+                    message: "Error"
+                });
+            } else {
+                if (data == null) {
+                    res.status = 200;
+                    res.json({
+                        status: true,
+                        message: "Not posted any cab."
+                    });
+                } else {
+                    // console.log(data.number_of_person);
+                    postCab.find({
+                        date: data.date,
+                        car_seats: { $gte: data.number_of_person },
+                        time: data.time,
+                    }, function (err, info) {
+                        if (err) {
+                            res.status = 401;
+                            res.json({
+                                status: false,
+                                message: "Error"
+                            });
+                        } else {
+                            if (info == null) {
+                                res.status = 200;
+                                res.json({
+                                    status: true,
+                                    message: "No cabs."
+                                });
+                            } else {
+                                getCheckLocation(info, data, function (ob) {
+                                    // console.log("printing data");
+                                    res.status = 200;
+                                    res.json({
+                                        status: true,
+                                        data: ob
+                                    });
+                                });
+                            }
+                        }
+                    })
+                }
+            }
+        });
+    }
+});
+
+getCheckLocation = function (info, data, callback) {
+    var place = [];
+    for (var i = 0; i < info.length; i++) {
+        console.log("still doing loop");
+        d = info[i];
+        var current_loc_url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + data.current_location + "&destinations=" + info[i].current_location + "&key=" + process.env.distance_matrix;
+        var destination_url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + data.destination + "&destinations=" + info[i].destination + "&key=" + process.env.distance_matrix;
+        unirest.get(current_loc_url)
+            .end(checkCurrent);
+        // console.log("cdscsdcsd");
+    }
+
+
+    function checkCurrent(cloc) {
+        if (cloc.body.rows[0].elements[0].distance.value < 500) {
+            unirest.get(destination_url)
+                .end(checkDestination);
+            // console.log("checkCurrent");
+        }
+    }
+
+    function checkDestination(dest) {
+        if (dest.body.rows[0].elements[0].distance.value < 2000) {
+            place.push(d);
+        }
+        // console.log("checkDestination", i);
+        if (i == info.length) {
+            // console.log("out of loop", i);
+            callback(place);
+        }
+        //  callback(place);
+    }
+}
 
 module.exports = router;
