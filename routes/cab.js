d1d8c90c222e0d166314bd7bb6fcf86bcbcdc190
mayankagg9722 <mayankagg9722@gmail.com>
2017-01-22 02:18:18 +0530

    /postcabinfo route for post cabs matching

--- routes/cab.js
+++ routes/cab.js
@@ -160,6 +160,68 @@ router.get('/cabrequested', function (req, res, next) {
     }
 });
 
+router.get('/postcabinfo', function (req, res, next) {
+    if (!req.user) {
+        res.status = 401;
+        res.json({
+            status: false,
+            redirect: 'login',
+            message: "Authentication Failed"
+        });
+    }
+    else {
+        postCab.findOne({ username: req.user.username }, function (err, data) {
+            if (err) {
+                res.status = 401;
+                res.json({
+                    status: false,
+                    message: "Error"
+                });
+            } else {
+                if (data == null) {
+                    res.status = 200;
+                    res.json({
+                        status: true,
+                        message: "Not posted any cab."
+                    });
+                } else {
+                    // console.log(data.number_of_person);
+                    getCab.find({
+                        date: data.date,
+                      number_of_person: { $gte: data.car_seats },
+                        time: data.time,
+                    }, function (err, info) {
+                        if (err) {
+                            res.status = 401;
+                            res.json({
+                                status: false,
+                                message: "Error"
+                            });
+                        } else {
+                            if (info == null) {
+                                res.status = 200;
+                                res.json({
+                                    status: true,
+                                    message: "No cabs."
+                                });
+                            } else {
+                                getCheckLocation(info, data, function (ob) {
+                                    // console.log("printing data");
+                                    res.status = 200;
+                                    res.json({
+                                        status: true,
+                                        data: ob
+                                    });
+                                });
+                            }
+                        }
+                    })
+                }
+            }
+        });
+    }
+});
+
 router.get('/getcabinfo', function (req, res, next) {
     if (!req.user) {
         res.status = 401;
@@ -222,10 +284,11 @@ router.get('/getcabinfo', function (req, res, next) {
     }
 });
 
+
 getCheckLocation = function (info, data, callback) {
     var place = [];
     for (var i = 0; i < info.length; i++) {
-        console.log("still doing loop");
+        // console.log("still doing loop");
         d = info[i];
         var current_loc_url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + data.current_location + "&destinations=" + info[i].current_location + "&key=" + process.env.distance_matrix;
         var destination_url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=" + data.destination + "&destinations=" + info[i].destination + "&key=" + process.env.distance_matrix;
